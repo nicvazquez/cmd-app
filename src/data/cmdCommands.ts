9380739359ac85655e6e3f5ae9815be642e5b7c9
Nicolas Vazquez <81201032+nicvazquez@users.noreply.github.com>
2023-10-01 19:21:57 -0300

    added 'datetime' command

--- src/data/cmdCommands.ts
+++ src/data/cmdCommands.ts
@@ -1,8 +1,12 @@
 import { type CommandType } from "@/types";
 
+const getCurrentDate = () => new Date().toLocaleDateString();
+const getCurrentTime = () => new Date().toLocaleTimeString();
+
 export const cmdCommands: CommandType = {
-	help: "available commands: date, time, user",
-	date: new Date().toLocaleDateString(),
-	time: new Date().toLocaleTimeString(),
+	help: "available commands: date, time, datetime, user",
+	date: getCurrentDate(),
+	time: getCurrentTime(),
+	datetime: `${getCurrentDate()} ${getCurrentTime()}`,
 	user: "root",
 };
